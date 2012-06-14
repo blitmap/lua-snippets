0cfa05ed20698fdc5f63a967b7b19d2bbfe12c84
Poggles <sir.pogsalot@gmail.com>
2012-06-14 16:50:07 -0700

    Clear up the cruft/over-engineering in pad.lua

--- pad.lua
+++ pad.lua
@@ -1,22 +1,27 @@
 #!/usr/bin/env lua
 
+local srep = string.rep
+
+module('pad')
+
 -- pad the left side
-string.lpad =
-	function (str, len, char)
-		return (char or ' '):rep(len - #str) .. str
+lpad =
+	function (s, len, c)
+		return srep(c or ' ', len - #s) .. s
 	end
 
 -- pad the right side
-string.rpad =
-	function (str, len, char)
-		return str .. (char or ' '):rep(len - #str)
+rpad =
+	function (s, len, c)
+		return s .. srep(c or ' ', len - #s)
 	end
 
--- pad on both sides (centering effect with left justification)
-string.pad =
-	function (str, len, char, just)
-		char = char or ' '
+-- pad on both sides (centering with left justification)
+pad =
+	function (s, len, c)
+		c = c or ' '
 
-		-- pad() prefers left-justified centering '  cat   ' -- 8 chars wide ('cat'):pad(8)
-		return str:rpad(math[just and 'floor' or 'ceil']((len + #str) / 2), char):lpad(len, char)
+		return lpad(rpad(s, (len / 2) + #s, c), len, c)
 	end
+
+return _M
