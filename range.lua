471c7d2efb231f86ee210f9c11d44d8d1f8ce905
Poggles <sir.pogsalot@gmail.com>
2012-06-14 21:16:17 -0700

    separate arg handling and setup from coroutine creation

--- range.lua
+++ range.lua
@@ -1,29 +1,79 @@
 #!/usr/bin/env lua
 
-local range =
-	function (num)
-		local count_to =
-			function (max)
-				for i = 1, max do
-					coroutine.yield(i)
-				end
-			end
+local setmeta = setmetatable
+local cyield  = coroutine.yield
+local cresume = coroutine.resume
+local ccreate = coroutine.create
+local select  = select
+
+module('range')
+
+local count =
+    function (i, to, inc)
+        while i ~= to do 
+            i = i + inc
+            -- should return two values for
+            -- some_table[i] = i usage
+            cyield(i, i) 
+        end
+    end
+
+-- our range() example separates the arg handling of
+-- the function from the yielding coroutine body (count())
+iter =
+    function (i, to, inc)
+        if not to then
+            to = i
+            i  = to == 0 or (to > 0 and 1 or -1)
+        end
+
+        inc = inc or (i < to and 1 or -1)
+
+        local co = ccreate(count)
+
+        return
+            function () -- iterator
+                return select(2, cresume(co, i - 1, to, inc)) -- select past the status
+            end
+
+        -- The above lines up to ccreate() could instead be written like so:
+        -- return cwrap(function () count(i - 1, to, inc) end)
+        -- coroutine.wrap() turns yielding functions into iterators (roughly)
+    end
+
+-- require('range') range() -> iter()
+return setmeta(_M, { __call = function (_, ...) return iter(...) end }) 
 
 --[[
-		local co = coroutine.create(count_to)
 
-		return
-			function () -- iterator
-				local status, res = coroutine.resume(co, num)
+-- Usage:
 
-				return res
-			end
---]]
-
-		-- ^ About the same as.
-		return coroutine.wrap(function () count_to(num) end)
-	end
+require('range')
 
 for i in range(10) do
-	print(i)
+    print(i)
 end
+
+debug.setmetatable(
+	function () end,
+	{
+		__index =
+			{
+				collect =
+					function (...)
+						local tmp = {}
+
+						for k, v in ... do
+							tmp[k] = v
+						end
+
+						return tmp
+					end
+			}
+	}
+)
+
+-- it's that easy. :p
+local some_table = range(3, 243, 9):collect()
+
+]]
