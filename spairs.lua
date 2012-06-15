861015bbde497cc729858b49cf08a5d2421e73e4
Poggles <sir.pogsalot@gmail.com>
2012-06-15 00:13:58 -0700

    Clean up spairs, modulize it for aquanight :p

--- spairs.lua
+++ spairs.lua
@@ -1,37 +1,44 @@
--- To be pedantic and snobbish,
--- this is technically a generator.
--- It "generates" an iterator.
-local spairs =
+local tsort   = table.sort
+local setmeta = setmetatable
+
+require('helpers')
+
+local tkeys = table.keys
+
+module('spairs')
+
+iter =
     function (tbl, sort_func)
-        local keys, x = {}, 0
+		local keys = tkeys(tbl)
         
-		-- Make our potentially unconstiguous index of
-		-- keys into a constiguous array of keys.
-        for index in pairs(tbl) do
-            x = x + 1
-            keys[x] = index
-        end 
-        
-        -- All the simple magic.
-        table.sort(keys, sort_func)
-        
-		-- Reset this so we can use it to
-		-- keep track of our "last key"
-		x = 0
-        
+        tsort(keys, sort_func)
+
+		local x = 0
+
         -- Our iterator function.
-        -- Upvalues: keys and x
+        -- Upvalues: tbl, keys, and x
         local spairs_iterator =
-            function (t)
+            function ()
 				x = x + 1
-                return keys[x], t[keys[x]]
+                return keys[x], tbl[keys[x]]
             end 
             
-        -- Return the iterator, the table,
-        -- and the starting index for the iterator
-        return spairs_iterator, tbl
+        return spairs_iterator
     end 
-    
+
+return setmetatable(_M, { __call = function (_, ...) return iter(...) end })
+
+--[[
+
+Example usage:
+
+-- Simple example with only number & string keys.
+for k, v in spairs({ 1, 2, [99] = '99', [23] = '23' }) do
+    print(k, v)
+end 
+
+print(srep('-', 40))
+
 local whatever =
 {
     [1]               = 'one',
@@ -42,14 +49,9 @@ local whatever =
     [function () end] = 'six'
 }   
 
--- Simple example with only number & string keys.
-for k, v in spairs({ 1, 2, [99] = '99', [23] = '23' }) do
-    print(k, v)
-end 
-
-print(string.rep('-', 40))
-
 -- Second example.
-for k, v in spairs(whatever, function (a, b) return tostring(a) < tostring(b) end) do
+for k, v in spairs(whatever, function (a, b) return tos(a) < tos(b) end) do
     print(k, v)
 end
+
+]]
