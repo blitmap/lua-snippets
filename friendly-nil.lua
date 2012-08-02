9a6a584d6b59181b65c49b31dd7041a89a32bd6a
Poggles <sir.pogsalot@gmail.com>
2012-08-02 06:07:56 +0000

    Add an example for making nil less... exceptional?

--- friendly-nil.lua
+++ friendly-nil.lua
@@ -2,26 +2,24 @@
 
 -- make nil friendlier
 
-do
-	-- these functions are named in the form of what they return
-	local none = function (      )            end
-	local rhs  = function (_, rhs) return rhs end
+-- these functions are named in the form of what they return
+local none = function (    )                            end
+local nnil = function (l, r) return l ~= nil and l or r end
 
-	local nil_mt =
-	{
-		__call   = none, -- call a nil value without pcall!
-		__index  = none, -- stop writing: return some_table and some_table[idx] or ...
-		__concat = rhs,  -- stop concatenating to an initial empty-string when iteratively building a long string!
-		__add    = rhs,  -- for arithmetic nil could be treated simply as 0 for all operations 0 + 3, 0 * 3, 0 ^ 3, ...
-		__unm    = function (    ) return    -0 end, -- sign is preserved even on 0
-		__mul    = function (_, y) return 0 * y end, -- again, to preserve signate
-		__sub    = function (_, y) return    -y end, -- just unary-minus the rhs
-		__div    = function (_, y) return 0 / y end, -- we can't make use of zero() because 0 / 0
-		__pow    = function (_, y) return 0 ^ y end,
-	}
+local nil_mt =
+{
+	__call   = none,                                       -- call a nil value without pcall and have it do nothing!
+	__index  = none,                                       -- stop writing: return some_table and some_table[idx] or ...
+	__concat = nnil,                                       -- stop concatenating to an initial empty-string when iteratively building a long string!
+	__add    = nnil,                                       -- stop adding nil to things...
+	__unm    = function (    ) return    - 0          end, -- sign is preserved even on 0
+	__mul    = function (l, r) return 0 *  nnil(l, r) end, -- again, to preserve signedness
+	__sub    = function (l, r) return    - nnil(l, r) end, -- just unary-minus the rhs
+	__div    = function (l, r) return 0 /  nnil(l, r) end, -- we can't make use of zero() because 0 / 0
+	__pow    = function (l, r) return 0 ^  nnil(l, r) end,
+}
 
-	debug.setmetatable(nil, nil_mt)
-end
+debug.setmetatable(nil, nil_mt)
 
 local a =  a()
 local b =  b['something']
