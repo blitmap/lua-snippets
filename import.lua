70ef128e2f6cca4fb41fdec608e80af182f6ced6
Poggles <sir.pogsalot@gmail.com>
2014-04-07 21:55:52 -0700

    make import more general

--- import.lua
+++ import.lua
@@ -1,69 +1,48 @@
 #!/usr/bin/env lua
 
---- import() -- creates global references to functions in a table,
---- by creating a new _ENV that "passes through" to the original _ENV
+local is_empty =
+	function (self)
+		return not next(self)
+	end
 
--- import(some_module)                                             -- import all functions as globals from some_module
--- import(some_module, 'prefix_')                                  -- import all functions as globals from some_module (whatever in some_module becomes prefix_whatever)
--- import(some_module, { 'only', 'these', 'functions' }             -- import some_module.only, some_module.these, and some_module.functions (selective import)
--- import(some_module, { 'only', 'these', 'functions' }, 'prefix_') -- ^ but with a prefix used when creating the global identifier
-
--- so we don't wind up creating "chained" _ENV's with import()
--- (creating multiple "inheriting" _ENV's with several calls to import())
-local envs = setmetatable({}, { __mode = 'kv' })
-
-local import =
-	function (t, ...)
-		require('debug')
-
-		local pref, keys
-
-		if select('#', ...) > 1 or (... ~= nil and type(...) == 'table') then
-			keys, pref = ...
-		else
-			pref, keys = ...
+table.import =
+	function (self, from, pref, keys)
+		if type(keys) == 'string' then
+			pref, keys = keys, pref
 		end
 
-		keys = keys or {}
 		pref = pref or ''
+		keys = keys or {}
 
-		-- table-empty check
-		if not next(keys) then
-			for k in pairs(t) do
-				table.insert(keys, k)
+		-- do we import everything?
+		if is_empty(keys) then
+			for k, v in pairs(from) do
+				self[pref .. k] = v
+			end
+		else
+			for _, k in pairs(keys) do
+				self[pref .. k] = from[k]
 			end
 		end
 
-		local new_env = envs[_ENV] or setmetatable({}, { __index = _ENV, __newindex = _ENV })
-
-		-- if envs[_ENV], we wind up adding to _ENV
-		for _, k in pairs(keys) do
-			rawset(new_env, pref .. k, t[k])
-		end
-
-		if not envs[new_env] then
-			envs[new_env] = new_env
-			debug.setupvalue(debug.getinfo(1, 'f').func, 1, new_env)
-			-- debug
-			print('env replaced')
-		else
-			-- debug
-			print('env not replaced')
-		end
+		return self
 	end
 
-import(string, { 'reverse', 'rep', 'sub' }, 's')
+assert(string.reverse == table.import({}, string     ).reverse )
+assert(string.reverse == table.import({}, string, 's').sreverse)
 
 local f =
 	function ()
-		import(io)
-		import(io, 'io_')
+		table.import(_ENV, io)
+		table.import(_ENV, io, 'io_')
 
 		write('hello thar!\r\n')
 		io_write('blah. ~\r\n')
 	end
 
+f()
+
+table.import(_ENV, string, 's', { 'reverse', 'rep', 'sub' })
+
 print(sreverse('cat'), srep('donut', 5), ssub('abcdefg', 3, 6))
 
-
-f()
